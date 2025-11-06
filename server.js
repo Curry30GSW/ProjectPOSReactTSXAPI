a14bb2e5d9642aacecfdc5ef55f2faf619cef3f8
Curry30GSW <eliteaxeso93@gmail.com>
2025-11-06 10:09:17 -0500

    Modulo de clientes semifinish

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "http://127.0.0.1:5502", // Debes usar el frontend correcto
+  origin: "http://localhost:5173", // Debes usar el frontend correcto
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
